cdb6502016483d5fbbaf1f7efb53b4da175241ad
Matheusclay <matheeus.silva9@hotmail.com>
2025-10-23 20:29:43 -0300

    issue resolve

--- etl/main.py
+++ etl/main.py
@@ -1,14 +1,9 @@
 import pandas as pd
+import os
 
-def load_data(file_path):
-    return pd.read_csv(file_path)
-
-def clean_data(data):
-    return data.dropna()
-
-def save_data(data, file_path):
-    data.to_csv(file_path, index=False)
+def load_data(file_name):
+    return pd.read_csv(os.path.join("data", file_name))
 
 if __name__ == "__main__":
-    data = load_data("data/campeonato-brasileiro-full.csv")
-    clean_data(data)
+    data = load_data("campeonato-brasileiro-full.csv")
+    print(data.head())
